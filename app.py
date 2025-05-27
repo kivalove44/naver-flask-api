8830b29fdd30c2b0cf00bdd7ccc2558954c90c7f
kivalove44 <kivalove@gaunet.co.kr>
2025-05-28 08:35:19 +0900

    fix: app.run with 0.0.0.0

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def search():
     return Response(json.dumps(simplified, ensure_ascii=False), mimetype='application/json')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
